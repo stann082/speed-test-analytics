ce4c9675d85a6af0d539bc8c2b90cbf7736f0206
Stan Bennett <stanley.bennett82@gmail.com>
2021-12-16 21:03:21 -0600

    Add Main method

--- runner/src/app/Program.cs
+++ runner/src/app/Program.cs
@@ -1,35 +1,54 @@
 using domain;
 using Serilog;
 using service;
-using worker;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
-    {
-        IConfiguration config = LoadConfiguration();
-        services.AddHostedService<Worker>()
-                .AddTransient<IAwsDynamoDbService, AwsDynamoDbService>()
-                .AddTransient<IProcessService, ProcessService>();
-    }).ConfigureLogging((hostContext, builder) =>
-    {
-        Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(hostContext.Configuration)
-                .WriteTo.NewRelicLogs(applicationName: "Speed Test", licenseKey: GetNewRelicApiKey())
-                .CreateLogger();
-    }).UseSerilog().Build();
+namespace worker;
 
-IConfiguration LoadConfiguration()
+public class Program
 {
-    IConfigurationBuilder builder = new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-    return builder.Build();
+
+    #region Public Methods
+
+    public static async Task Main(string[] args)
+    {
+        IHost host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services =>
+            {
+                IConfiguration config = LoadConfiguration();
+                services.AddHostedService<Worker>()
+                        .AddTransient<IAwsDynamoDbService, AwsDynamoDbService>()
+                        .AddTransient<IProcessService, ProcessService>();
+            }).ConfigureLogging((hostContext, builder) =>
+            {
+                Log.Logger = new LoggerConfiguration()
+                        .ReadFrom.Configuration(hostContext.Configuration)
+                        .WriteTo.NewRelicLogs(applicationName: "Speed Test", licenseKey: GetNewRelicApiKey())
+                        .CreateLogger();
+            }).UseSerilog().Build();
+
+        await host.RunAsync();
+    }
+
+    #endregion
+
+    #region Helper Methods
+
+    private static IConfiguration LoadConfiguration()
+    {
+        IConfigurationBuilder builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        return builder.Build();
+    }
+
+    private static string GetNewRelicApiKey()
+    {
+        return EnvironmentVariableProvider.GetEnvironmentVariable("NewRelicApiKey");
+    }
+
+    #endregion
+
 }
 
-string GetNewRelicApiKey()
-{
-    return EnvironmentVariableProvider.GetEnvironmentVariable("NewRelicApiKey");
-}
 
-await host.RunAsync();
 
